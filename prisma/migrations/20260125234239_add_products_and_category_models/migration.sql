eea5237d01dbdcb68ba941e590921191aed6bec5
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-26 06:52:27 +0700

    db: add product and category models

---
+++ prisma/migrations/20260125234239_add_products_and_category_models/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "categoryId" TEXT,
+ADD COLUMN     "description" TEXT,
+ADD COLUMN     "imageUrl" TEXT;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
